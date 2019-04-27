d53deedc9088ab48abba10058e066853c43c5e48
Roman Bulinski <aboandrus@gmail.com>
2019-04-27 17:52:32 +0200

    Implemented code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,38 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
-
     public static String printerError(String s) {
-        // your code
+
+        String[] strchar = s.split("");
+        List<String> strcharList = Arrays.asList( strchar );
+
+        long amount = strcharList.stream()
+                .filter( n-> n.equals("n")
+                        || n.equals("o")
+                        || n.equals("p")
+                        || n.equals("q")
+                        || n.equals("r")
+                        || n.equals("s")
+                        || n.equals("t")
+                        || n.equals("u")
+                        || n.equals("v")
+                        || n.equals("w")
+                        || n.equals("x")
+                        || n.equals("y")
+                        || n.equals("z"))
+                .count();
+
+        String result = "";
+        result = result + amount +"/"+ s.length();
+
+        return  result;
+
     }
+
 
 
 
--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainTest {
+
+
+    @Test
+    public void tst1(){
+        assertEquals(  "0/5", Main.printerError("abcde")   );
+    }
+
+
 }
